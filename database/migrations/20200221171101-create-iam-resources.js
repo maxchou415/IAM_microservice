0b429a5cf8d11d6008b76e888265463830b0e8a4
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-02-22 03:42:15 +0800

    - Create IAM Resources table

---
+++ database/migrations/20200221171101-create-iam-resources.js
@@ -0,0 +1,30 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('iam_resources', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      path: {
+        type: Sequelize.STRING
+      },
+      method: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('iam_resources')
+  }
+}
