67b347d2fced79bcffa5521a43b08e7068e950f5
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-02-22 03:45:39 +0800

    - add customized error handler

---
+++ infra/utils/responseHandlers/errorMessageMap.js
@@ -0,0 +1,7 @@
+module.exports = {
+  AUTH_FAILED: { status: 401, message: 'AUTH_FAILED' },
+
+  FETCH_HOUSE_FAILURE: { status: 400, message: 'FETCH_HOUSE_FAILURE' },
+
+  BAD_REQUEST: { status: 400, message: 'BAD_REQUEST' }
+}
