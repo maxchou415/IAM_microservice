f4d0bc164dbbf77e1f3d72d1d47fc5322d2ad3f9
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-02-22 03:41:41 +0800

    - Create IAM Role table

---
+++ database/migrations/20200221170559-create-iam-roles.js
@@ -0,0 +1,27 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('iam_roles', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('iam_roles')
+  }
+}
