01d8b13f2fb564ab1adeb86f7a957a9f29639ce6
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-02-22 03:41:53 +0800

    - Create IAM Groups table

---
+++ database/migrations/20200221170935-create-iam-groups.js
@@ -0,0 +1,27 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('iam_groups', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('iam_groups')
+  }
+}
