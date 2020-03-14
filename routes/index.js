28551974f0317b21ae713f59c5fd1febe5293409
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-03-14 23:12:07 +0800

    - One more newline to split the creation api and verification api

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/group/:groupId/roles', controllers.getIAMRolesByIAMGroupId)
 
 router.post('/role', controllers.newIAMRole)
 router.post('/group', controllers.newIAMGroup)
+
 router.post('/verify', controllers.verifyByIAMGroupId)
 
 module.exports = router
