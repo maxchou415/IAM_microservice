9318910b232177525db0eb6797d74a6757db1067
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-03-14 23:06:25 +0800

    - Add database seeder to add default test data for app initialize

--- database/seeders/20200221181718-add-iam-role.js
+++ database/seeders/20200221181718-add-iam-role.js
@@ -3,21 +3,17 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.bulkInsert('iam_roles', [{
+      queryInterface.bulkInsert('IAMRoles', [{
         name: 'AccessAdminConsole',
         // Naming rule: Action(use HTTP method as priority if any)ResourceName ex. GETUsers.
-
-        id: 1, // Give a value for the next seed process. ***NEVER DO THIS IN PRODUCTION***.
 
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '), // FUCK THIS
         updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
       }], {}),
 
-      queryInterface.bulkInsert('iam_roles', [{
+      queryInterface.bulkInsert('IAMRoles', [{
         name: 'GETConsoleIAMGroups',
         // Naming rule: Action(use HTTP method as priority if any)ResourceName ex. GETUsers.
-
-        id: 2, // Give a value for the next seed process. ***NEVER DO THIS IN PRODUCTION***.
 
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '), // FUCK THIS
         updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
@@ -26,6 +22,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('iam_roles', null, {})
+    return queryInterface.bulkDelete('IAMRoles', null, {})
   }
 }
--- database/seeders/20200221182415-add-iam-role-into-iam-group.js
+++ database/seeders/20200221182415-add-iam-role-into-iam-group.js
@@ -3,21 +3,17 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.bulkInsert('iam_role_iam_group', [{
-        id: 1,
-
-        iam_role_id: 1,
-        iam_group_id: 1,
+      queryInterface.bulkInsert('IAMRoleIAMGroup', [{
+        IAMRoleId: 1,
+        IAMGroupId: 1,
 
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
         updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
       }], {}),
 
-      queryInterface.bulkInsert('iam_role_iam_group', [{
-        id: 2,
-
-        iam_role_id: 2,
-        iam_group_id: 1,
+      queryInterface.bulkInsert('IAMRoleIAMGroup', [{
+        IAMRoleId: 2,
+        IAMGroupId: 1,
 
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
         updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
@@ -26,6 +22,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('iam_role_iam_group', null, {})
+    return queryInterface.bulkDelete('IAMRoleIAMGroup', null, {})
   }
 }
