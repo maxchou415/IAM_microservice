c3123b31fb97a4e5e51333a1f16cc0f17ec79ced
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-03-14 23:10:45 +0800

    - Add newIAMGroup service

--- routes/services/newIAMGroup.js
+++ routes/services/newIAMGroup.js
@@ -1,3 +1,17 @@
-module.exports = async (ctx, next) => {
+const { IAMGroups, sequelize } = require('@database/models')
 
+module.exports = async (data) => {
+  const IAMGroupData = {
+    name: data.name
+  }
+
+  const IAMGroupRelatedIAMs = data.iams
+
+  const saveIAMGroupData = await sequelize.transaction(async (t) => {
+    const IAMGroup = await IAMGroups.create(IAMGroupData, { transaction: t })
+    await IAMGroup.addRoles(IAMGroupRelatedIAMs, { transaction: t })
+    return IAMGroup
+  })
+
+  return saveIAMGroupData
 }
