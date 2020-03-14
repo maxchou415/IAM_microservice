19ad12f3877415e60eb57e8cce9fdde3be5c51f8
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-03-14 23:10:13 +0800

    - Remove try catch handling in service

--- routes/services/getIAMRolesByIAMGroupId.js
+++ routes/services/getIAMRolesByIAMGroupId.js
@@ -2,24 +2,20 @@ const models = require('../../database/models')
 
 module.exports = async (IAMGroupId) => {
   let data
-  try {
-    data = await models.IAMGroups.findAll({
-      where: {
-        id: IAMGroupId
+  data = await models.IAMGroups.findAll({
+    where: {
+      id: IAMGroupId
+    },
+    include: [{
+      model: models.IAMRoles,
+      attributes: ['name', 'id'],
+      required: true,
+      through: {
+        attributes: []
       },
-      include: [{
-        model: models.IAMRoles,
-        attributes: ['name', 'id'],
-        required: true,
-        through: {
-          attributes: []
-        },
-        as: 'roles'
-      }]
-    })
-  } catch (error) {
-    console.log(error)
-  }
+      as: 'roles'
+    }]
+  })
 
   return data
 }
