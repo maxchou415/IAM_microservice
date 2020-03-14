845baab429611ce19fac26307f7d9cd64ffccd99
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2020-03-14 23:09:53 +0800

    - Add newIAMGroup Controller

--- routes/controllers/newIAMGroup.js
+++ routes/controllers/newIAMGroup.js
@@ -1,3 +1,28 @@
-module.exports = async (ctx, next) => {
+const { newIAMGroup } = require('../services')
+const errorMessageMap = require('../../infra/utils/responseHandlers/errorMessageMap')
 
+module.exports = async (ctx, next) => {
+  const wantedData = ctx.request.body
+
+  if (!wantedData || !wantedData.name || !wantedData.roles || !wantedData.roles.length) {
+    ctx.throw(errorMessageMap.NEW_IAM_GROUP_WANTED_PARAMETER_LOSTED)
+    return
+  }
+
+  const newIAMGroupData = {
+    name: wantedData.name,
+    iams: wantedData.roles
+  }
+
+  try {
+    await newIAMGroup(newIAMGroupData)
+  } catch (error) {
+    ctx.throw(errorMessageMap.NEW_IAM_GROUP_FAIULRED_ON_SAVING)
+    return
+  }
+
+  ctx.status = 200
+  ctx.body = {
+    status: 'success'
+  }
 }
